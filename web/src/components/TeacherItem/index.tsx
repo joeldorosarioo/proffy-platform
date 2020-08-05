9ca3ca6ef0d9209755fe368e066569ba03223688
Joel Oliveira <joeldorosarioo@gmail.com>
2020-08-05 13:31:35 -0300

    Proffy pages

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+	return (
+		<article className="teacher-item">
+			<header>
+				<img src="https://avatars3.githubusercontent.com/u/54811476?s=460&u=0711d59032fa8547deb4fbe05a8792988a13d47d&v=4" alt="Joel Oliveira" />
+
+				<div>
+					<strong>Joel Oliveira</strong>
+					<span>Química</span>
+				</div>
+			</header>
+
+			<p>
+				Entusiasta das melhores tecnologias de química avançada.
+						<br /><br />
+						Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+					</p>
+
+			<footer>
+				<p>
+					Preço/hora
+							<strong>R$ 80,00</strong>
+				</p>
+
+				<button type="button">
+					<img src={whatsappIcon} alt="WhatsAApp" />
+							Entrar em contato
+						</button>
+			</footer>
+		</article>
+	);
+}
+
+export default TeacherItem;
