3ba97cd175e67c20165e83b489d6748946440530
Joel Oliveira <joeldorosarioo@gmail.com>
2020-08-07 16:58:05 -0300

    Connection of the application with the API

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,37 +1,50 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+	avatar: string
+	bio: string
+	cost: number
+	id: number
+	name: string
+	subject: string
+	whatsapp: string
+};
+
+interface TeacherItemProps {
+	teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function createNewConnection() {
+		api.post('connections', {
+			user_id: teacher.id
+		});
+	}
+
 	return (
 		<article className="teacher-item">
 			<header>
-				<img src="https://avatars3.githubusercontent.com/u/54811476?s=460&u=0711d59032fa8547deb4fbe05a8792988a13d47d&v=4" alt="Joel Oliveira" />
+				<img src={ teacher.avatar } alt={ teacher.name } />
 
 				<div>
-					<strong>Joel Oliveira</strong>
-					<span>Química</span>
+					<strong>{ teacher.name }</strong>
+					<span>{ teacher.subject }</span>
 				</div>
 			</header>
 
-			<p>
-				Entusiasta das melhores tecnologias de química avançada.
-						<br /><br />
-						Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-					</p>
+			<p>{ teacher.bio }</p>
 
 			<footer>
-				<p>
-					Preço/hora
-							<strong>R$ 80,00</strong>
-				</p>
+				<p>Preço/hora <strong>{ teacher.cost }</strong></p>
 
-				<button type="button">
-					<img src={whatsappIcon} alt="WhatsAApp" />
-							Entrar em contato
-						</button>
+				<a target="_blank" onClick={ createNewConnection } href={`https://wa.me/${ teacher.whatsapp }`}>
+					<img src={whatsappIcon} alt="WhatsAApp" /> Entrar em contato
+				</a>
 			</footer>
 		</article>
 	);
